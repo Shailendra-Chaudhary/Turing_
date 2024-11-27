114e03054b01ebedbb7a254f7ebe9bcc7a70464c
SHAILENDRA <41490477+Shailendra-Chaudhary@users.noreply.github.com>
2024-11-27 16:42:08 +0530

    Create 366375_Turn_2_Model_A.cpp

---
+++ 366375_Turn_2_Model_A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int counter = 0;
+    for (int i = 0; i < 10; ++i) {
+        counter++;
+        std::cout << "Counter: " << counter << std::endl;
+    }
+    return 0;
+}
