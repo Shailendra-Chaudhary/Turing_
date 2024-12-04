2c8ea9747ee4ec0e11cbef964081da14b0c67eb2
SHAILENDRA <41490477+Shailendra-Chaudhary@users.noreply.github.com>
2024-12-04 17:16:40 +0530

    Create 391262_Turn_2_Model_B.cpp

---
+++ 391262_Turn_2_Model_B.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "my_math.h"
+
+TEST(MyMathTest, AddTest) {
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(-1, 1), 0);
+}
+
+TEST(MyMathTest, SubtractTest) {
+    EXPECT_EQ(subtract(5, 3), 2);
+    EXPECT_EQ(subtract(2, 2), 0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
