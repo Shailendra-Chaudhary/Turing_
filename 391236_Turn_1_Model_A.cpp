0568f37d4a3d543425a8944d052c261c903efb1b
SHAILENDRA <41490477+Shailendra-Chaudhary@users.noreply.github.com>
2024-12-04 13:44:28 +0530

    Create 391236_Turn_1_Model_A.cpp

---
+++ 391236_Turn_1_Model_A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "a * b = " << a * b << std::endl;
+    return 0;
+}
