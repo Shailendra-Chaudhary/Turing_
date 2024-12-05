7e3c6019a0060f33686829499af890fecc0d2e20
SHAILENDRA <41490477+Shailendra-Chaudhary@users.noreply.github.com>
2024-12-05 20:09:46 +0530

    Update 391281_Turn_2_Model_B.cpp

--- 391281_Turn_2_Model_B.cpp
+++ 391281_Turn_2_Model_B.cpp
@@ -1,38 +1,79 @@
 #include <iostream>
-#include <thread>
 #include <vector>
-#include <chrono>
+#include <thread>
+#include <mutex>
+#include <memory>
 
-// Worker function to mimic memory-intensive work
-void worker(size_t id) {
-    // Simulate some memory allocation and deallocation
-    while (true) {
-        std::vector<int> data(1000); // Allocate some memory
-        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulate work
+// A simple memory allocation profiler
+class MemoryProfiler {
+public:
+    static void allocate(size_t size) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        totalAllocated_ += size;
+        std::cout << "Allocated " << size << " bytes. Total allocated: " << totalAllocated_ << " bytes\n";
     }
+
+    static void deallocate(size_t size) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        totalAllocated_ -= size;
+        std::cout << "Deallocated " << size << " bytes. Total allocated: " << totalAllocated_ << " bytes\n";
+    }
+
+private:
+    static size_t totalAllocated_;
+    static std::mutex mutex_;
+};
+
+size_t MemoryProfiler::totalAllocated_ = 0;
+std::mutex MemoryProfiler::mutex_;
+
+// Overriding new and delete to capture memory allocation and deallocation
+void* operator new(size_t size) {
+    void* ptr = std::malloc(size);
+    if (ptr) {
+        MemoryProfiler::allocate(size);
+    }
+    return ptr;
+}
+
+void operator delete(void* ptr) noexcept {
+    if (ptr) {
+        // Size of deallocating memory can't be passed to the delete operator, hence not tracked here.
+        MemoryProfiler::deallocate(0);
+    }
+    std::free(ptr);
+}
+
+// Implementing a concurrent task
+void taskFunction(int threadId) {
+    std::cout << "Thread " << threadId << " is starting.\n";
+
+    // Simulating memory allocation
+    // Using a 1000-int vector as an example of dynamic allocation
+    std::vector<int> vec(1000, threadId); // Memory allocated here
+
+    // Simulate some work
+    for (int& num : vec) {
+        num *= 2;
+    }
+
+    std::cout << "Thread " << threadId << " has finished work.\n";
 }
 
 int main() {
-    std::cout << "Starting concurrent program...\n";
-
-    size_t numThreads = 4;
+    const std::size_t numThreads = 4;
     std::vector<std::thread> threads;
 
-    // Launch threads
-    for (size_t i = 0; i < numThreads; ++i) {
-        threads.emplace_back(worker, i);
+    // Start threads
+    for (std::size_t i = 0; i < numThreads; ++i) {
+        threads.emplace_back(taskFunction, i);
     }
 
-    // Allow the threads to run for some time
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    // Conclude program execution after enough profiling data is generated
+    // Join threads
     for (auto& t : threads) {
-        if (t.joinable()) {
-            t.detach();  // We use detach here since we are terminating forcefully
-        }
+        t.join();
     }
 
-    std::cout << "Concurrent program completed.\n";
+    std::cout << "All threads have finished execution.\n";
     return 0;
 }
