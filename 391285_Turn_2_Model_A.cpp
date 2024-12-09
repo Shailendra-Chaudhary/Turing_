49f56accb4d5178ca0bc1c6bf41c21f6c2898b9e
SHAILENDRA <41490477+Shailendra-Chaudhary@users.noreply.github.com>
2024-12-09 18:05:34 +0530

    Update 391285_Turn_2_Model_A.cpp

--- 391285_Turn_2_Model_A.cpp
+++ 391285_Turn_2_Model_A.cpp
@@ -1,37 +1,29 @@
-// feature.h
+#include "gtest/gtest.h"
+#include "variant_example.h"
 
-#pragma once
-
-#ifdef FEATURE_FLAG
-inline std::string getFeatureValue() {
-    return "Feature enabled";
-}
-#else
-inline std::string getFeatureValue() {
-    return "Feature disabled";
-}
+// Test for C++17 or later compiler
+TEST(VariantTestCpp17, IntBehavior) {
+#if __cplusplus >= 201703L
+    VariantType variant = getVariant();
+    ASSERT_EQ(getIntFromVariant(variant), 42);
+    EXPECT_THROW(getStringFromVariant(variant), std::bad_variant_access);
 #endif
-// test_feature.cpp
-
-#include <gtest/gtest.h>
-#include "feature.h"
-
-TEST(FeatureTest, TestFeatureDisabled) {
-    #undef FEATURE_FLAG
-    EXPECT_EQ(getFeatureValue(), "Feature disabled");
-    #ifdef FEATURE_FLAG
-    #error FEATURE_FLAG should not be defined here
-    #endif
 }
 
-TEST(FeatureTest, TestFeatureEnabled) {
-    #define FEATURE_FLAG
-    EXPECT_EQ(getFeatureValue(), "Feature enabled");
-    #ifndef FEATURE_FLAG
-    #error FEATURE_FLAG should be defined here
-    #endif
+// Simulate a C++11 compiler environment (if not already in C++11 or earlier)
+// In practice, you would need to set up the compiler appropriately for C++11 or earlier.
+// Below example simulates using conditional compilation directives
+TEST(VariantTestCpp11, IntBehavior) {
+#if !defined(__cplusplus) || __cplusplus < 201402L
+    #define __cplusplus 201103L  // Simulating C++11
+    VariantType variant = getVariant();
+    ASSERT_EQ(getIntFromVariant(variant), 42);
+    EXPECT_THROW(getStringFromVariant(variant), std::bad_variant_access);
+    #undef __cplusplus
+#endif
 }
 
+// Main function to run the tests
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
